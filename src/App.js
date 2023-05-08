08aee9a98bfb48cba3c95885342ded00ff27bf4f
ed from dav's laptop <ed0207@mymail.com>
2023-05-08 16:45:06 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import {Navbar, Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="lg" sticky="top">
+        <h1>testing</h1>
+        <Button>B button</Button>
+      </Navbar>
+      <header className="App-header">
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
