07f038796421eedf766ec6debf5a542a40d84cea
ed from dav's laptop <newed0207@outlook.com>
2023-09-14 17:00:18 -0700

    implementing routes for future project expansion

---
+++ src/routes/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import AboutMe from '../components/AboutMe';
+import ProjectCard from '../components/ProjectCard';
+import Footer from '../components/Footer';
+
+function Home(){
+    return(<div className='Home'>
+        <Navbar name="Edward's Portfolio"></Navbar>
+        <Hero text="Welcome"></Hero>
+        <AboutMe></AboutMe>
+        <ProjectCard></ProjectCard>
+        <Footer></Footer>
+    </div>)
+}
+
+export default Home;
---
+++ src/routes/Test.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Test(){
+    return(<div>
+        <h1>Testing page</h1>
+    </div>)
+}
+
+export default Test;
