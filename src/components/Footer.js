cb6b276d6b8f70b4228642228a05c2bb2c8238b3
ed <ed@Ubuntu.myguest.virtualbox.org>
2023-12-18 14:01:38 -0800

    Footer icon update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import "./Footer.css"
+import {FaHome, FaPhone, FaTwitter, FaLinkedin} from "react-icons/fa"
 
 function Footer(){
     return(<div className='Footer'>
-        <h3>Footer</h3>
         <div className='Footer-content'>
+                <FaHome/> 
                 <h5>4588 Dubbert St</h5>
                 <h5>Richmond, B.C.</h5>
                 <h5>V6X 0M1</h5>
+                <FaPhone/>
                 <h5>(778)513-4244</h5>
         </div>
         <div className='Footer-content'>
-                <h5>LinkedIn</h5>
-                <h5>Twitter</h5>
+                <a href=""><h5> <FaLinkedin/> LinkedIn</h5></a>
         </div>
         <div className='disclaimer'>
         
