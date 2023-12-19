046438ea1ba917e9665db2958a761c0ffad02a53
Ed on MSI 2023 <newed0207@outlook.com>
2023-12-19 14:30:10 -0800

    container + element size adjustment

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,10 +9,9 @@ function AboutMe(props){
             <h2>Aboue Me</h2>
             <img src={headshot} className='headshot' alt='Edward Chen'/>
             <div className='aboutmeText'>
-                <body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sapien faucibus et molestie ac feugiat. Lorem sed risus ultricies tristique nulla aliquet enim. Magna ac placerat vestibulum lectus mauris. Arcu cursus vitae congue mauris. Amet mauris commodo quis imperdiet massa tincidunt nunc. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Amet tellus cras adipiscing enim eu turpis egestas. Dolor sit amet consectetur adipiscing elit ut. Amet risus nullam eget felis eget. Ut enim blandit volutpat maecenas. Viverra ipsum nunc aliquet bibendum enim facilisis. Fringilla est ullamcorper eget nulla. Diam vulputate ut pharetra sit amet. Dignissim convallis aenean et tortor at risus. Nisl pretium fusce id velit ut tortor pretium viverra. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Blandit volutpat maecenas volutpat blandit aliquam etiam.</body>
-            </div>
-            <div className='recentProjects'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa sapien faucibus et molestie ac feugiat. Lorem sed risus ultricies tristique nulla aliquet enim. Magna ac placerat vestibulum lectus mauris. Arcu cursus vitae congue mauris. Amet mauris commodo quis imperdiet massa tincidunt nunc. Nunc aliquet bibendum enim facilisis gravida neque convallis a cras. Amet tellus cras adipiscing enim eu turpis egestas. Dolor sit amet consectetur adipiscing elit ut. Amet risus nullam eget felis eget. Ut enim blandit volutpat maecenas. Viverra ipsum nunc aliquet bibendum enim facilisis. Fringilla est ullamcorper eget nulla. Diam vulputate ut pharetra sit amet. Dignissim convallis aenean et tortor at risus. Nisl pretium fusce id velit ut tortor pretium viverra. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Commodo nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Blandit volutpat maecenas volutpat blandit aliquam etiam.
 
+                </p>
             </div>
     </div>);
 }
